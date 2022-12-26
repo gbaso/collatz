c10fdcb5e7ec38bf7f0f335c47179f66d923143f
Giacomo Baso <ganondolf@gmail.com>
2022-12-26 01:30:05 +0100

    simplify service lookup

--- src/main/java/com/example/collatz/CollatzApplication.java
+++ src/main/java/com/example/collatz/CollatzApplication.java
@@ -1,10 +1,11 @@
 package com.example.collatz;
 
+import java.util.Map;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,14 +20,14 @@ public class CollatzApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(ApplicationContext context, ServiceProperties service) {
+	CommandLineRunner runner(Map<String, CollatzService> serviceMap, ServiceProperties service) {
 		return args -> {
 			if (args.length < 1) {
 				log.info("pass the starting number you dolt!");
 				System.exit(1);
 			}
 			int max = Integer.parseInt(args[0]);
-			var collatzService = context.getBean(service.type().getName(), CollatzService.class);
+			var collatzService = serviceMap.get(service.type().getName());
 			var info = collatzService.findMaxLength(max);
 			log.info("Max length {} for starting value(s): {}", info.length(), info.starts());
 		};
